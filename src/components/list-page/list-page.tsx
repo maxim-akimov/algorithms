3d93bc94e29bb0721d0e6a8f93d8c1bbede04174
Maksim Akimov <79629719220@ya.ru>
2023-11-10 16:34:48 +0300

    Очередь

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,10 +1,166 @@
-import React from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import styles from "./list.module.css";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { useForm } from "../../hooks/useForm";
 
 export const ListPage: React.FC = () => {
+  const defaultList = undefined; //TODO
+
+
+  const [formElementsState, setFormElementsState] = useState({
+    valueInput: {
+      isDisabled: false,
+    },
+    indexInput: {
+      isDisabled: false,
+    },
+    addHeadBtn: {
+      isDisabled: true,
+      isProcess: false
+    },
+    addTailBtn: {
+      isDisabled: true,
+      isProcess: false
+    },
+    addByIndexBtn: {
+      isDisabled: true,
+      isProcess: false
+    },
+    removeHeadBtn: {
+      isDisabled: true,
+      isProcess: false
+    },
+    removeTailBtn: {
+      isDisabled: true,
+      isProcess: false
+    },
+    removeByIndexBtn: {
+      isDisabled: true,
+      isProcess: false
+    },
+  });
+
+  const [form, handleChange, setValues] = useForm();
+
+
+  useEffect(() => {
+    if (form.value && form.value.length > 0) {
+      setFormElementsState({
+        ...formElementsState,
+        addHeadBtn: {
+          ...formElementsState.addHeadBtn,
+          isDisabled: false,
+        },
+        addTailBtn: {
+          ...formElementsState.addTailBtn,
+          isDisabled: false,
+        },
+      })
+    } else {
+      setFormElementsState({
+        ...formElementsState,
+        addHeadBtn: {
+          ...formElementsState.addHeadBtn,
+          isDisabled: true,
+        },
+        addTailBtn: {
+          ...formElementsState.addTailBtn,
+          isDisabled: true,
+        },
+      })
+    }
+    /*
+    if (form.value && form.value.length > 0
+      && form.index && form.index.length > 0) {
+      setFormElementsState({
+        ...formElementsState,
+        addByIndexBtn: {
+          ...formElementsState.addByIndexBtn,
+          isDisabled: false,
+        },
+      })
+    } else {
+      setFormElementsState({
+        ...formElementsState,
+        addByIndexBtn: {
+          ...formElementsState.addByIndexBtn,
+          isDisabled: true,
+        },
+      })
+    }
+
+     */
+    console.log(formElementsState);
+  }, [form])
+
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  }
+
+
+  const handleAddHeadClick = (e: FormEvent) => {
+
+  }
+
+
+  const handleAddTailClick = (e: FormEvent) => {
+
+  }
+
+
+  const handleAddByIndexClick = (e: FormEvent) => {
+
+  }
+
+
+  const handleRemoveHeadClick = (e: FormEvent) => {
+
+  }
+
+
+  const handleRemoveTailClick = (e: FormEvent) => {
+
+  }
+
+
+  const handleRemoveByIndexClick = (e: FormEvent) => {
+
+  }
+
+
   return (
     <SolutionLayout title="Связный список">
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <Input name={'value'} type={'text'} placeholder={'Введите значение'} maxLength={4} isLimitText={true}
+               extraClass={styles.input} onChange={handleChange} value={form.value || ''}
+               disabled={formElementsState.valueInput.isDisabled}/>
+        <Button name={'addHeadBtn'} text={'Добавить в head'} type={'button'}
+                isLoader={formElementsState.addHeadBtn.isProcess}
+                disabled={formElementsState.addHeadBtn.isDisabled} onClick={handleAddHeadClick}/>
+        <Button name={'addTailBtn'} text={'Добавить в tail'} type={'button'}
+                isLoader={formElementsState.addTailBtn.isProcess}
+                disabled={formElementsState.addTailBtn.isDisabled} onClick={handleAddTailClick}/>
+        <Button name={'removeHeadBtn'} text={'Удалить из head'} type={'button'}
+                isLoader={formElementsState.removeTailBtn.isProcess}
+                disabled={formElementsState.removeHeadBtn.isDisabled} onClick={handleRemoveHeadClick}/>
+        <Button name={'removeTailBtn'} text={'Удалить из tail'} type={'button'}
+                isLoader={formElementsState.removeTailBtn.isProcess}
+                disabled={formElementsState.removeTailBtn.isDisabled} onClick={handleRemoveTailClick}/>
 
+        <Input name={'index'} type={'text'} placeholder={'Введите индекс'} maxLength={1} min={0}
+               extraClass={styles.input} onChange={handleChange} value={form.index || ''}
+               disabled={formElementsState.indexInput.isDisabled}/>
+        <Button text={'Добавить по индексу'} type={'button'} isLoader={formElementsState.addByIndexBtn.isProcess}
+                disabled={formElementsState.addByIndexBtn.isDisabled} onClick={handleAddByIndexClick}
+                extraClass={styles.add_index_btn}/>
+        <Button text={'Удалить по индексу'} type={'button'} isLoader={formElementsState.removeByIndexBtn.isProcess}
+                disabled={formElementsState.removeByIndexBtn.isDisabled} onClick={handleRemoveByIndexClick}
+                extraClass={styles.delete_index_btn}/>
+
+      </form>
     </SolutionLayout>
   );
 };
--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -17,19 +17,19 @@ interface IQueue<T> {
   getSize: () => number;
   getElements: () => (T | null)[];
   clear: () => void;
-  getHead: () => number;
-  getTail: () => number;
+  getHead: () => number | null;
+  getTail: () => number | null;
 }
 
-type HeadTailStates = {
+type HighlightState = {
   index: number | null;
   state: ElementStates;
 }
 
 export class Queue<T> implements IQueue<T> {
   private readonly container: (T | null)[] = [];
-  private head = 0;
-  private tail = 0;
+  private head: number = 0;
+  private tail: number = 0;
   private readonly size: number = 0;
   private length: number = 0;
 
@@ -44,9 +44,7 @@ export class Queue<T> implements IQueue<T> {
     }
 
     this.container[this.tail % this.size] = item;
-    if (this.tail + 1 > this.size) {
-      this.tail = 0;
-    } else if (this.tail === 0) {
+    if (this.tail > this.size - 1) {
       this.tail = 1;
     } else {
       this.tail++;
@@ -66,13 +64,18 @@ export class Queue<T> implements IQueue<T> {
       this.head++;
     }
     this.length--;
+
+    if (this.length === 0) {
+      this.head = 0;
+      this.tail =0;
+    }
   };
 
   peak = (): T | null => {
     if (this.isEmpty()) {
       throw new Error("No elements in the queue");
     }
-    return this.container[this.head % this.size] || null;
+    return this.container[(this.head || 0) % this.size] || null;
   };
 
   isEmpty = () => this.length === 0;
@@ -85,6 +88,8 @@ export class Queue<T> implements IQueue<T> {
 
   clear = () => {
     this.length = 0;
+    this.head = 0;
+    this.tail = 0;
 
     for (let i = 0; i < this.container.length; i++) {
       this.container[i] = null;
@@ -109,8 +114,9 @@ export const QueuePage: React.FC = () => {
   const [isDisabledClear, setDisabledClear] = useState(true);
 
   const [queueState, setQueueState] = useState<(string | number | null)[]>([]);
-  const [head, setHead] = useState<HeadTailStates>({ index: null, state: ElementStates.Changing });
-  const [tail, setTail] = useState<HeadTailStates>({ index: null, state: ElementStates.Changing });
+  const [head, setHead] = useState<number | null>(null);
+  const [tail, setTail] = useState<number | null>(null);
+  const [highlight, setHighlight] = useState<HighlightState>({ index: null, state: ElementStates.Changing });
 
   const [form, handleChange, setValues] = useForm();
 
@@ -161,11 +167,14 @@ export const QueuePage: React.FC = () => {
 
     setQueueState([...elements]);
 
-    setHead({ index: headIndex, state: ElementStates.Default })
-    setTail({ index: tailIndex - 1, state: ElementStates.Default })
+    setHead(headIndex);
+    setTail(tailIndex - 1);
+    setHighlight({ index: tailIndex - 1, state: ElementStates.Changing} );
 
     setTimeout(() => {
-      //setLasElState({ index: elements.length - 1, state: ElementStates.Default})
+      setTail(tailIndex - 1)
+      setHighlight({ index: tailIndex - 1, state: ElementStates.Default} );
+
       setProcessAdd(false);
     }, SHORT_DELAY_IN_MS)
 
@@ -179,17 +188,28 @@ export const QueuePage: React.FC = () => {
     setProcessDelete(true);
 
     const elements = queue.getElements();
-    const headIndex = queue.getHead();
-    const tailIndex = queue.getTail();
-
-    //setLasElState({ index: elements.length - 1, state: ElementStates.Changing})
+    let deletedElementIndex = queue.getHead();
+    setHighlight({ index: deletedElementIndex, state: ElementStates.Changing} );
 
     setTimeout(() => {
       queue.dequeue();
+
+      let headIndex;
+      let tailIndex;
+
+      if (queue.getLength() === 0) {
+        headIndex = null;
+        tailIndex = null;
+      } else {
+        headIndex = queue.getHead();
+        tailIndex = queue.getTail() - 1;
+      }
+
       setQueueState([...elements]);
-      setHead({ index: headIndex + 1, state: ElementStates.Default })
-      setTail({ index: tailIndex - 1, state: ElementStates.Default })
-      //setLasElState({ index: elements.length - 1, state: ElementStates.Default})
+      setHead(headIndex)
+      setTail(tailIndex)
+      setHighlight({ index: headIndex, state: ElementStates.Default} );
+
       setProcessDelete(false);
     }, SHORT_DELAY_IN_MS)
   }
@@ -217,9 +237,9 @@ export const QueuePage: React.FC = () => {
       </form>
       <section className={styles.result}>
         {queueState.map((el, i) => (
-          <Circle key={i} index={i} state={(i === head.index) ? head.state : ElementStates.Default}
-                  head={(i === head.index) ? 'head' : ''}
-                  tail={(i === tail.index) ? 'tail' : ''} letter={(el) ? el.toString() : ''}/>
+          <Circle key={i} index={i} state={(i === highlight.index) ? highlight.state : ElementStates.Default}
+                  head={(i === head) ? 'head' : ''}
+                  tail={(i === tail) ? 'tail' : ''} letter={(el) ? el.toString() : ''}/>
         ))}
       </section>
     </SolutionLayout>
