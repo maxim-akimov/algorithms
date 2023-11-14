28b6271bc99490256dc40c3d17f73afca139f567
Maksim Akimov <79629719220@ya.ru>
2023-11-14 16:43:46 +0300

    Связный список (завершено)

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -11,7 +11,7 @@ import { SHORT_DELAY_IN_MS } from "../../constants/delays";
 import { delay } from "../../utils/utils.ts";
 
 
-export class Node<T> {
+class Node<T> {
   value: T | ''
   next: Node<T> | null
 
@@ -24,7 +24,7 @@ export class Node<T> {
 interface ILinkedList<T> {
   append: (element: T) => void;
   insertAt: (element: T, position: number) => void;
-  remove: (val: T) => Node<T> | null;
+  remove: (val: T) => void;
   getSize: () => number;
   getElements: () => void;
 }
@@ -80,6 +80,8 @@ class LinkedList<T> implements ILinkedList<T> {
 
       this.size++;
     }
+    console.log(this.getElements())
+
   }
 
   append(element: T) {
@@ -97,6 +99,7 @@ class LinkedList<T> implements ILinkedList<T> {
       current.next = node;
     }
     this.size++;
+    console.log(this.getElements())
   }
 
   remove(val: T) {
@@ -113,8 +116,10 @@ class LinkedList<T> implements ILinkedList<T> {
       }
       curr = nextNode;
     }
+    this.head = dummyHead.next;
+    console.log(this.getElements())
 
-    return dummyHead.next;
+    //return dummyHead.next;
   };
 
   getSize() {
@@ -139,11 +144,6 @@ export const ListPage: React.FC = () => {
   }, []);
 
 
-  for (let i = 0; i < 4; i++) {
-    linkedList.append(Math.floor(Math.random() * 100))
-  }
-
-
   const initialBtnState = { isDisabled: true, isProcess: false };
   const initialElementsState = {
     index: null,
@@ -163,7 +163,7 @@ export const ListPage: React.FC = () => {
   const [form, handleChange, setValues] = useForm();
 
   // Стейты для реализации анимации
-  const [listState, setListState] = useState<(number | string | null)[]>(linkedList.getElements());
+  const [listState, setListState] = useState<(number | string)[]>([]);
   // Стейт элемента, подлежащего вставке
   const [insertElement, setInsertElement] = useState<ElementsActionsState>(initialElementsState);
   // Стейт элемента, подлежащего удалению
@@ -175,6 +175,14 @@ export const ListPage: React.FC = () => {
     elements: [],
     state: ElementStates.Changing
   })
+
+
+  useEffect(() => {
+    for (let i = 0; i < 4; i++) {
+      linkedList.append(Math.floor(Math.random() * 100))
+    }
+    setListState(linkedList.getElements());
+  }, []);
 
 
   useEffect(() => {
@@ -195,29 +203,26 @@ export const ListPage: React.FC = () => {
     }
 
     if (form.index && form.index.length > 0 && listState.length > 0) {
-      setRemoveByIndexBtnState({ ...removeByIndexBtnState, isDisabled: false});
-    } else {
-      setRemoveByIndexBtnState({ ...removeByIndexBtnState, isDisabled: true});
-    }
-  }, [form])
-
-
-  useEffect(() => {
-    if (listState.length > 0) {
-      setRemoveHeadBtnState({ ...removeHeadBtnState, isDisabled: false });
-      setRemoveTailBtnState({ ...removeTailBtnState, isDisabled: false });
       setRemoveByIndexBtnState({ ...removeByIndexBtnState, isDisabled: false });
     } else {
-      setRemoveHeadBtnState({ ...removeHeadBtnState, isDisabled: true });
-      setRemoveTailBtnState({ ...removeTailBtnState, isDisabled: true });
-      setRemoveByIndexBtnState({ ...removeByIndexBtnState, isDisabled: true });
-    }
+      setRemoveByIndexBtnState({ ...removeByIndexBtnState, isDisabled: true });    }
 
     if (listState.length > 6) {
       setDisabledValue(true);
-      setDisabledIndex(true);
+    } else {
+      setDisabledValue(false);
     }
-  }, [listState])
+
+    if (listState.length > 0) {
+      setRemoveHeadBtnState({ ...removeHeadBtnState, isDisabled: false });
+      setRemoveTailBtnState({ ...removeTailBtnState, isDisabled: false });
+    } else {
+      setRemoveHeadBtnState({ ...removeHeadBtnState, isDisabled: true });
+      setRemoveTailBtnState({ ...removeTailBtnState, isDisabled: true });
+    }
+  }, [form, listState])
+
+
 
 
   const handleSubmit = (e: FormEvent) => {
@@ -225,65 +230,48 @@ export const ListPage: React.FC = () => {
   }
 
 
-  const handleAddClick = (e: FormEvent, index?: number) => {
+  const handleAddClick = (e: FormEvent, place?: 'head' | 'tail' | number) => {
     e.preventDefault();
+    let index: number = 0;
 
-    if (index === undefined) {
+    if (place === undefined) {
       setAddByIndexBtnState({ ...addByIndexBtnState, isProcess: true });
       index = Number(form.index);
-    } else {
+    } else if (typeof place === 'number') {
+      setAddByIndexBtnState({ ...addByIndexBtnState, isProcess: true });
+      index = place;
+    } else if (place === 'head') {
       setAddHeadBtnState({ ...addHeadBtnState, isProcess: true });
+    } else if (place === 'tail') {
+      setAddTailBtnState({ ...addHeadBtnState, isProcess: true });
+      index = listState.length;
     }
 
-    linkedList.insertAt(form.value, index);
+    if (place === 'tail') {
+      linkedList.append(form.value);
+    } else {
+      linkedList.insertAt(form.value, index);
+    }
 
-    setInsertElement({ index: index, letter: form.value, state: ElementStates.Changing });
+    setInsertElement({ index: (place === 'tail')? index - 1 : index, letter: form.value, state: ElementStates.Changing });
 
     setTimeout(() => {
-      const newListState = [...listState];
-      newListState.splice(index || 0, 0, form.value);
-
-      setListState([...newListState]);
+      setListState([...linkedList.getElements()]);
       setInsertElement(initialElementsState)
-      setActiveElement({ index: index as number, state: ElementStates.Modified });
+      setActiveElement({ index: index, state: ElementStates.Modified });
     }, SHORT_DELAY_IN_MS);
 
     setTimeout(() => {
-      setActiveElement({ index: index as number, state: ElementStates.Default });
+      setActiveElement({ index: index, state: ElementStates.Default });
       setAddByIndexBtnState({ ...addByIndexBtnState, isDisabled: true, isProcess: false });
       setAddHeadBtnState({ ...addHeadBtnState, isProcess: false, isDisabled: true });
+      setAddTailBtnState({ ...addTailBtnState, isProcess: false, isDisabled: true });
+
     }, SHORT_DELAY_IN_MS * 2);
 
-
-    setValues({ value: '' });
-
-
-  }
-
-
-  const handleAddTailClick = (e: FormEvent) => {
-    e.preventDefault();
-
-    setAddTailBtnState({ ...addTailBtnState, isProcess: true });
-
-    linkedList.append(form.value);
-
-    setInsertElement({ index: listState.length - 1, letter: form.value, state: ElementStates.Changing });
-
     setTimeout(() => {
-      setListState([...listState, form.value]);
-      setInsertElement(initialElementsState)
-
-    }, SHORT_DELAY_IN_MS);
-
-    setTimeout(() => {
-      setActiveElement({ index: listState.length, state: ElementStates.Modified });
-    }, SHORT_DELAY_IN_MS);
-
-    setTimeout(() => {
-      setActiveElement({ index: listState.length, state: ElementStates.Default });
-      setAddTailBtnState({ ...addTailBtnState, isDisabled: true, isProcess: false });
-    }, SHORT_DELAY_IN_MS * 2);
+      setActiveElement({ index: null, state: ElementStates.Default });
+    }, SHORT_DELAY_IN_MS * 3);
 
     setValues({ value: '' });
   }
@@ -292,14 +280,17 @@ export const ListPage: React.FC = () => {
   const handleRemoveClick = async (e: FormEvent, index?: number) => {
     e.preventDefault();
 
-    setRemoveByIndexBtnState({ ...removeByIndexBtnState, isProcess: true});
-
     if (index === undefined) {
       index = Number(form.index);
+      setRemoveByIndexBtnState({ ...removeByIndexBtnState, isProcess: true});
+    } else if (index === 0) {
+      setRemoveHeadBtnState({ ...removeHeadBtnState, isProcess: true });
+    } else {
+      setRemoveTailBtnState({ ...removeTailBtnState, isProcess: true });
+
     }
 
     const elements = [];
-
     for (let ind = 0; ind <= index; ind++) {
       elements.push(ind);
       setSelectedElements({ ...selectedElements, elements: [...elements] })
@@ -309,13 +300,17 @@ export const ListPage: React.FC = () => {
     setDeleteElement({ index: index, letter: listState[index] + '', state: ElementStates.Changing });
     setListState([...listState.map((el, i) => (i === index) ? el = '' : el)])
 
-    await setTimeout(() => {
-      setListState([...listState.filter((el, i) => (i !== index))]);
+    setTimeout(() => {
+      linkedList.remove(listState[index || 0])
+      setListState([...linkedList.getElements()]);
       setSelectedElements({ ...selectedElements, elements: []});
       setDeleteElement(initialElementsState);
+
+      setRemoveByIndexBtnState({ ...removeByIndexBtnState, isProcess: false});
+      setRemoveHeadBtnState({ ...removeHeadBtnState, isProcess: false });
+      setRemoveTailBtnState({ ...removeTailBtnState, isProcess: false });
     }, SHORT_DELAY_IN_MS)
 
-    setRemoveByIndexBtnState({ isDisabled: true, isProcess: false});
     setValues({ index: '' });
   }
 
@@ -328,10 +323,10 @@ export const ListPage: React.FC = () => {
                disabled={isDisabledValue}/>
         <Button name={'addHeadBtn'} text={'Добавить в head'} type={'button'}
                 isLoader={addHeadBtnState.isProcess}
-                disabled={addHeadBtnState.isDisabled} onClick={(e) => handleAddClick(e, 0)}/>
+                disabled={addHeadBtnState.isDisabled} onClick={(e) => handleAddClick(e, 'head')}/>
         <Button name={'addTailBtn'} text={'Добавить в tail'} type={'button'}
                 isLoader={addTailBtnState.isProcess}
-                disabled={addTailBtnState.isDisabled} onClick={handleAddTailClick}/>
+                disabled={addTailBtnState.isDisabled} onClick={(e) => handleAddClick(e, 'tail')}/>
         <Button name={'removeHeadBtn'} text={'Удалить из head'} type={'button'}
                 isLoader={removeHeadBtnState.isProcess}
                 disabled={removeHeadBtnState.isDisabled} onClick={(e) => handleRemoveClick(e, 0)}/>
